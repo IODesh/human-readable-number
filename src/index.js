2c65055ec64dd9f4f0e1261dab55947e6c4e3277
Igor Desh <desh_gambler@mail.ru>
2021-07-21 19:18:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function toReadable (number) {
-  
+        //let numberToWords = require('number-to-words');
+        //let numRead = numberToWords.toWords(number).split('');
+        //for (let i = 0; i < numRead.length; i++) {
+        //if (numRead[i] === '-') {
+        //    numRead[i] = ' ';
+        //    };
+        //};
+        //return numRead.join('');
+
+        
+        units = [' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+        tens = [' ', ' ', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+        scales = [' ', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+	
+    let arr = [' ', ' ', ' '];
+	
+	if (number === 0)
+		{
+		arr[0] = 'zero';
+		}
+	else if (number < 20 && number !== 0)
+		{
+		arr[0] = units[number];
+		}
+	else if (number % 100 === 0) 
+	 	{
+		arr[0] = scales[number / 100];
+		
+		}
+	else if (number % 100 < 20 && Math.floor(number / 100) !== 0)
+		{
+		 arr[0] = scales[Math.floor(number / 100)];
+	     arr[1] = units[number % 100];
+		}
+	else if (number % 100 >= 20 && Math.floor(number / 100) !== 0)
+		{
+		 arr[0] = scales[Math.floor(number / 100)];
+	     arr[1] = tens[Math.floor(number % 100/ 10)];
+		 arr[2] = units[number % 10];
+		}
+	else if (number < 100 && number > 19)
+		{
+		 arr[0] = tens[Math.floor(number % 100/ 10)];
+		 arr[1] = units[number % 10];
+		}
+	else if (Math.floor(number % 100) === 0 && number % 10 === 0)
+		{
+		 arr[0] = tens[number/10];
+		}
+ //console.log(arr);
+return arr.reduce((a, b) => a +' '+b);
 }
+
+//let n = 0;
+//let result = toReadable(n);
+//console.log(result);
